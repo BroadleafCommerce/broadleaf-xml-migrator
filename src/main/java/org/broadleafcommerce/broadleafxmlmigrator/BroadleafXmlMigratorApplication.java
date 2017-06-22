f3160717e1b2b2c159dc2349921bac6e30163c73
Jay Aisenbrey <jaisenbrey@broadleafcommerce.com>
2017-06-21 19:01:46 -0500

    Refactored the basic example to something more reuseable

--- src/main/java/org/broadleafcommerce/broadleafxmlmigrator/BroadleafXmlMigratorApplication.java
+++ src/main/java/org/broadleafcommerce/broadleafxmlmigrator/BroadleafXmlMigratorApplication.java
@@ -1,6 +1,8 @@
 package org.broadleafcommerce.broadleafxmlmigrator;
 
 import org.springframework.beans.factory.config.ListFactoryBean;
+import org.springframework.beans.factory.config.MapFactoryBean;
+import org.springframework.beans.factory.config.SetFactoryBean;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
@@ -15,15 +17,16 @@ import com.jcabi.xml.XMLDocument;
 
 import java.io.IOException;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Properties;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @SpringBootApplication
@@ -37,44 +40,74 @@ public class BroadleafXmlMigratorApplication implements ApplicationRunner {
     public void run(ApplicationArguments args) throws Exception {
         // TODO: take in qualifier as command line argument
         String qualifier = "client";
-        
-        
         ClassPathResource appctx = new ClassPathResource("applicationContext.xml");
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
         DocumentBuilder db = dbf.newDocumentBuilder();
         Document document = db.parse(appctx.getInputStream());
+        ExecutionArguments executionArgs = new ExecutionArguments("/beans/bean[@id='messageSource']/property[@name='basenames']/list/value", 
+                                                        "/beans/bean[@id='messageSource']", "messageSourceBaseNames-client", 
+                                                        "messageSourceBaseNames", CollectionType.LIST, MergeType.LATE);
+        insertAndReplaceWithMerge(document, executionArgs);
+        System.out.println(new XMLDocument(document).toString());
+    }
+    
+    protected void insertAndReplaceWithMerge(Document document, ExecutionArguments args) throws XPathExpressionException {
         XPath evaluator = XPathFactory.newInstance().newXPath();
-        NodeList vals = (NodeList) evaluator.evaluate("/beans/bean[@id='messageSource']/property[@name='basenames']/list/value", document, XPathConstants.NODESET);
-        
-        String newBeanId = "messageSourceBaseNames-client";
-        Element newListBean = document.createElement("bean");
-        newListBean.setAttribute("id", newBeanId);
-        newListBean.setAttribute("class", ListFactoryBean.class.getName());
-        Element list = document.createElement("list");
+        NodeList vals = (NodeList) evaluator.evaluate(args.getCollectionXpath(), document, XPathConstants.NODESET);
+        Element newCollectionBean = document.createElement("bean");
+        newCollectionBean.setAttribute("id", args.getNewBeanId());
+        newCollectionBean.setAttribute("class", args.getCollectionType().getCollectionClass());
+        Element newCollection = document.createElement(args.getCollectionType().collectionName);
         for (int i = 0; i < vals.getLength(); i++) {
-            list.appendChild(vals.item(i));
+            newCollection.appendChild(vals.item(i));
         }
-        newListBean.appendChild(list);
+        newCollectionBean.appendChild(newCollection);
         
         Element mergeBean = document.createElement("bean");
-        mergeBean.setAttribute("class", "org.broadleafcommerce.common.extensibility.context.merge.LateStageMergeBeanPostProcessor");
+        mergeBean.setAttribute("class", args.getMergeType().getMergeClass());
         Element source = document.createElement("property");
         source.setAttribute("name", "sourceRef");
-        source.setAttribute("value", newBeanId);
+        source.setAttribute("value", args.getNewBeanId());
         mergeBean.appendChild(source);
         Element target = document.createElement("property");
         target.setAttribute("name", "targetRef");
-        target.setAttribute("value", "messageSourceBaseNames");
+        target.setAttribute("value", args.getTargetBeanId());
         mergeBean.appendChild(target);
         
-        Node oldBeanDef = (Node) evaluator.evaluate("/beans/bean[@id='messageSource']", document, XPathConstants.NODE);
+        Node oldBeanDef = (Node) evaluator.evaluate(args.getBeanXpath(), document, XPathConstants.NODE);
         
-        // the first child should always be <beans>
-        document.getFirstChild().insertBefore(newListBean, oldBeanDef);
+        document.getFirstChild().insertBefore(newCollectionBean, oldBeanDef);
         document.getFirstChild().insertBefore(mergeBean, oldBeanDef);
         document.getFirstChild().removeChild(oldBeanDef);
-        
-        System.out.println(new XMLDocument(document).toString());
+    }
+    
+    @Data
+    @AllArgsConstructor
+    public static class ExecutionArguments {
+        private String collectionXpath;
+        private String beanXpath;
+        private String newBeanId;
+        private String targetBeanId;
+        private CollectionType collectionType;
+        private MergeType mergeType;
+    }
+    
+    @Data
+    @AllArgsConstructor
+    public static class MergeType {
+        public static final MergeType LATE = new MergeType("org.broadleafcommerce.common.extensibility.context.merge.LateStageMergeBeanPostProcessor");
+        public static final MergeType EARLY = new MergeType("org.broadleafcommerce.common.extensibility.context.merge.EarlyStageMergeBeanPostProcessor");
+        private String mergeClass;
+    }
+    
+    @Data
+    @AllArgsConstructor
+    public static class CollectionType {
+        public static final CollectionType LIST = new CollectionType("list", ListFactoryBean.class.getName());
+        public static final CollectionType MAP = new CollectionType("map", MapFactoryBean.class.getName());
+        public static final CollectionType SET = new CollectionType("set", SetFactoryBean.class.getName());
+        private String collectionName;
+        private String collectionClass;
     }
     
     protected void loadHandlerProperties() throws IOException {
@@ -86,10 +119,4 @@ public class BroadleafXmlMigratorApplication implements ApplicationRunner {
         }
     }
     
-    @Data
-    public static class Handler {
-        private String parentOldXpath;
-        private List<String> mergePointXpaths;
-        private String newMergePointBeanId;
-    }
 }
